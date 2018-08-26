98d43d97543976df4127ac21419a06a14a7116f9
qxy0731 <qxy0731@163.com>
2018-08-26 21:54:28 +1000

    migration

--- project_source_code/comp9900/pending/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from property import models
-# Register your models here.
-admin.site.register(models.Property)
-admin.site.register(models.Images)
-admin.site.register(models.Address)
