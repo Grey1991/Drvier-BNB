51d7a46302a7718962a33e46ddfbeabe961304a8
Grey Wu <grey1991ss@gmail.com>
2018-08-28 07:49:56 +1000

    add pagination

--- comp9900/Property/search.py
+++ comp9900/Property/search.py
@@ -1,24 +1,40 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from Property import forms, models
-from Property import forms
 from django.db.models import Q
-import time
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 
 def simple_search(request):
     template = 'property_list.html'
     query = request.GET.get('q')
-    results = models.Property.objects.filter(Q(province__icontains=query) |
+    property_list = models.Property.objects.filter(Q(province__icontains=query) |
                                              Q(city__icontains=query) |
                                              Q(state__icontains=query) |
                                              Q(address__icontains=query)
                                              # Q(postcode__exact=int(query))
                                              ).distinct()
 
+    # 分页-----------------
+    paginator = Paginator(property_list, 10)
+    page = request.GET.get('page')
+
+    try:
+        properties = paginator.page(page)
+    except PageNotAnInteger:
+        properties = paginator.page(1)
+    except EmptyPage:
+        properties = paginator.page(paginator.num_pages)
+
+    # index = properties.number - 1
+    # max_index = len(paginator.page_range)
+    # start_index = index - 5 if index >= 5 else 0
+    # end_index = index + 5 if index <= max_index else max_index
+    # page_range = paginator.page_range[start_index:end_index]
+
     context = {
-        'properties': results
+        'properties': properties,
+        # 'page_range': page_range
     }
 
     return render(request, template, context)
