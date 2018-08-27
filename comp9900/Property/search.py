53e1209b38cf66c3cde9eb19cb71a8b90ff97634
Grey Wu <grey1991ss@gmail.com>
2018-08-27 23:12:52 +1000

    add search

---
+++ comp9900/Property/search.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from Property import forms, models
+from Property import forms
+from django.db.models import Q
+import time
+
+
+
+def simple_search(request):
+    template = 'search_list.html'
+    query = request.GET.get('q')
+    results = models.Property.objects.filter(Q(province__icontains=query) |
+                                             Q(city__icontains=query) |
+                                             Q(state__icontains=query) |
+                                             Q(address__icontains=query) |
+                                             Q(postcode__exact=int(query))
+                                             ).distinct()
