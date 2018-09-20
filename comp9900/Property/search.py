6cc95eb2e43acec26fa162b64dbd87d5f0ba92b1
Grey Wu <grey1991ss@gmail.com>
2018-09-21 02:06:22 +1000

    add date search and modify base and property_detail

--- comp9900/Property/search.py
+++ comp9900/Property/search.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from Property import forms, models
+from PendingAndBooking import models as Pmodels
 from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+import re
 
 
 
@@ -27,7 +29,11 @@ def simple_search(request):
     if not check_in:
         print ("no check_in")
     else:
-        print("yes")
+        check_in = re.sub(r'(\d{2})-(\d{2})-(\d{4})', '\\3-\\2-\\1', check_in)
+    if not check_out:
+        print ("no check_out")
+    else:
+        check_out = re.sub(r'(\d{2})-(\d{2})-(\d{4})', '\\3-\\2-\\1', check_out)
     if not capacity:
         capacity = 0
     else:
@@ -44,15 +50,36 @@ def simple_search(request):
 
     print('check_in: {},check_out: {},type: {},capacity: {},city: {},price: {}'.format(check_in,check_out,type,capacity,city,price),sep='\n')
 
+    if not check_in and not check_out:
+        booked_properties = []
+    elif not check_in:
+        booked_properties = Pmodels.TransAndReview.objects.filter(Q(start_time__lt=check_out)
+                                                             ).values_list('pid',flat=True).distinct()
+    elif not check_out:
+        booked_properties = Pmodels.TransAndReview.objects.filter(Q(end_time__gt=check_in)
+                                                             ).values_list('pid',flat=True).distinct()
+    else:
+        booked_properties = Pmodels.TransAndReview.objects.filter(Q(start_time__lt=check_out) &
+                                                                 Q(end_time__gt=check_in)
+                                                                 ).values_list('pid',flat=True).distinct()
+
+    for x in booked_properties:
+        print('booked id is ',x)
+
 
     property_list = models.Property.objects.filter(Q(city__icontains=city) &
-                                             Q(types_property__icontains=type) &
-                                             Q(price__gte=price) &
-                                             Q(capacity__gte=capacity)
-                                             # Q(postcode__exact=int(query))
-                                             ).distinct()
+                                                   Q(types_property__icontains=type) &
+                                                   Q(price__gte=price) &
+                                                   Q(capacity__gte=capacity)
+                                                   ).distinct().exclude(id__in=booked_properties)
+
+
+
+    # ====================== sort part====================================
 
     property_list = sorted(property_list, key=lambda x: x.price, reverse=False)
+
+    # ====================== end sort part =================================
 
     # 分页-----------------
     paginator = Paginator(property_list, 10)
